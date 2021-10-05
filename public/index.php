12da7598ca5cd2a074aa5e7f9d3fd7a70b4ed417
Sergey98Am <serg98barca@gmail.com>
2021-10-05 18:17:51 +0400

    added view file for rendering template, and used it in methods

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 require '../vendor/autoload.php';
 
+//Twig_Autoloader::register();
+
 error_reporting(E_ALL);
 set_error_handler('Core\Error::errorHandler');
 set_exception_handler('Core\Error::exceptionHandler');
