a3674a7d942a1fe89467796994b1340516c6d61d
Sergey98Am <serg98barca@gmail.com>
2021-10-12 12:57:43 +0400

    added Auth class and moved sessions there

---
+++ App/Auth.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+class Auth
+{
+    public static function login($user)
+    {
+        session_regenerate_id(true);
+        $_SESSION['user_id'] = $user->id;
+    }
+
+    public static function logout()
+    {
+        $_SESSION = [];
+
+        if (ini_get('session.use_cookies')) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params['path'],
+                $params['domain'],
+                $params['secure'],
+                $params['httponly']
+            );
+        }
+
+        session_destroy();
+    }
+
+    public static function isLoggedIn()
+    {
+        return isset($_SESSION['user_id']);
+    }
+}
